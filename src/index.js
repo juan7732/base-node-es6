6a1cdf8ff237ca514af517326fa6dc624d1da233
Juan Ruiz Valencia <juan7732@gmail.com>
2020-03-11 23:13:39 -0500

    added import and fetch example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import fetch from 'node-fetch';
+
 console.log('It works!')
 
 console.log('Just change the name of index.js for your script and you are ready to use the latest es6 features.')
+
+fetch('https://postman-echo.com/get?foo1=bar1&foo2=bar2', { method: 'GET'})
+    .then(res => res.json()) // expecting a json response
+    .then(json => console.log(json));
